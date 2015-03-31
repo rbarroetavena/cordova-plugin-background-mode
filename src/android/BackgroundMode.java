26332c73b36607533b94c0b652b2b46eeaad41e1
rbarroetavena <rbarroetavena@gmail.com>
2015-03-31 16:45:12 -0300

    remove flag FLAG_KEEP_SCREEN_ON

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -39,6 +39,8 @@ import android.os.PowerManager;
 import android.util.Log;
 import android.view.WindowManager;
 
+import com.ionicframework.background381706.MainActivity;
+
 public class BackgroundMode extends CordovaPlugin {
 
     // Event types for callbacks
@@ -200,7 +202,7 @@ public class BackgroundMode extends CordovaPlugin {
             Runnable runTask = new Runnable() {
                 public void run(){
                     cordova.getActivity().getWindow().addFlags(
-                            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                            //WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
                             WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                             WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                             WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
