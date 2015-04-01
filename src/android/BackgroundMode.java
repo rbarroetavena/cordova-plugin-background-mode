47a02da3bfe8525d1f3845e599cb73da1de0d902
rbarroetavena <rbarroetavena@gmail.com>
2015-04-01 13:22:12 -0300

    clear all flags

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -223,8 +223,11 @@ public class BackgroundMode extends CordovaPlugin {
         Runnable runTask = new Runnable() {
             public void run(){
                 cordova.getActivity().getWindow().clearFlags(
-                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                    );
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                );
             }
         };
         activity.runOnUiThread(runTask);
